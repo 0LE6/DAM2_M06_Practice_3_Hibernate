821717c4862494f49c4e13f6a745c5a85f53f997
Oleg <kharenkoleg@gmail.com>
2023-11-29 17:31:23 +0100

    public class DAOManagerHibernateImpl implements DAOManager{

---
+++ DAM2_M06_UF2_Hibernate_1/src/main/java/DAO/DAOManagerHibernateImpl.java
@@ -0,0 +1,25 @@
+package DAO;
+
+import MODEL.Empleat;
+
+public class DAOManagerHibernateImpl implements DAOManager{
+
+	@Override
+	public void createEmployee(Empleat e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public double getMaxSalary() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	@Override
+	public void close() throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
