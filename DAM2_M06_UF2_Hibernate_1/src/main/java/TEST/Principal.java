323ba99cb64330f9270d38792fc33787d97401be
Oleg <kharenkoleg@gmail.com>
2023-11-29 16:31:14 +0100

    añadido Principal.java

--- DAM2_M06_UF2_Hibernate_1/src/main/java/TEST/Principal.java
+++ DAM2_M06_UF2_Hibernate_1/src/main/java/TEST/Principal.java
@@ -1,5 +1,36 @@
 package TEST;
 
+
+import java.sql.Date;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+import MODEL.Empleat;
+
+
 public class Principal {
+
+	public static void main(String[] args) {
+		
+		  EntityManagerFactory emfactory = Persistence.createEntityManagerFactory("ORMEmployee");
+	      EntityManager entitymanager = emfactory.createEntityManager( );
+	      EntityTransaction t =null;
+	      t= entitymanager.getTransaction();
+	      t.begin();
+	      Empleat e = new Empleat( ); 
+	      e.setId((long)9996);
+	      e.setCognom("CHIC");
+	      e.setNom("FERRAN");
+	      e.setSalari( 40000D );
+	      e.setDataNaixement(Date.valueOf("1968-11-30"));
+	      entitymanager.persist( e );
+	      t.commit();
+
+	      entitymanager.close( );
+	      emfactory.close( );
+
+	}
 
 }
