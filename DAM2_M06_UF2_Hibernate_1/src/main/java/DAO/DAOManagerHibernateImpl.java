6004fc75cec880da67fa9a093abd476636e5cbce
Oleg <kharenkoleg@gmail.com>
2023-11-29 20:11:34 +0100

    en principio, todo funcional

--- DAM2_M06_UF2_Hibernate_1/src/main/java/DAO/DAOManagerHibernateImpl.java
+++ DAM2_M06_UF2_Hibernate_1/src/main/java/DAO/DAOManagerHibernateImpl.java
@@ -4,9 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-
 import javax.persistence.Query;
-
 import java.sql.Date;
 
 import MODEL.Empleat;
